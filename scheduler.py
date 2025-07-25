c1e9069589df9fe312986f0acc1016c52d8be198
AlexeevMikhail <alekseevmichail94@gmail.com>
2025-07-25 18:25:26 +0300

    Исправил логику обработки поездок

--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,23 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+import pytz
 from utils.database import close_expired_trips
 
 def start_scheduler():
-    scheduler = AsyncIOScheduler(timezone="Europe/Paris")
-    # Пн–Чт в 18:00
+    moscow_tz = pytz.timezone("Europe/Moscow")
+    scheduler = AsyncIOScheduler(timezone=moscow_tz)
+
+    # Пн–Чт в 18:00 по МСК
     scheduler.add_job(
         close_expired_trips,
-        trigger="cron",
-        day_of_week="mon-thu",
-        hour=18, minute=0
+        trigger=CronTrigger(day_of_week='mon-thu', hour=18, minute=0, timezone=moscow_tz)
     )
-    # Пт 16:45
+
+    # Пт в 16:45 по МСК
     scheduler.add_job(
         close_expired_trips,
-        trigger="cron",
-        day_of_week="fri",
-        hour=16, minute=45
+        trigger=CronTrigger(day_of_week='fri', hour=16, minute=45, timezone=moscow_tz)
     )
+
     scheduler.start()
     print("✅ Планировщик успешно запущен.")
